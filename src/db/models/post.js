618c52b4fd380739725c9cd30553aabd7c190a63
usemykey <useyourkey@gmail.com>
2024-12-06 23:05:49 +0800

    rest lession commit

---
+++ src/db/models/post.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose'
+
+// const postSchema = new Schema({
+//     title: { type: String, required: true },
+//     author: String,
+//     contents: String,
+//     tags: [String],
+// })
+
+const postSchema = new Schema({
+    title: String,
+    author: String,
+    contents: String,
+    tags: [String],
+    },
+    { timestamps: true },
+)
+
+export const Post = mongoose.model('post', postSchema)
